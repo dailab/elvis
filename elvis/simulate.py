e50654c212bb826edc812cb34d8bc1eb88413d37
19MM97 <moritz.markschlaeger@web.de>
2020-08-23 14:59:51 +0200

    Added functions to handle car arrivals, disconnect/connect vehicles

--- elvis/simulate.py
+++ elvis/simulate.py
@@ -1,134 +1,187 @@
+"""Simulation of the behavior of the charging infrastructure.
+
+TODO:
+- Should the waiting queue rather be a public variable instead of always passing
+    and returning it from function to function?
+- Faster way to find available conneciton_points necessary? Currently loop over all until one
+     that is available is found."""
+
 import datetime
-import math
-import numpy as np
-import time
+import queue
 
-import distribution
+from elvis.charging_event_generator import create_charging_events
+from elvis.set_up_infrastructure import set_up_charging_points
 
 
-def time_stamp_to_hours(time_stamps):
-    """Calculates for each time stamp in a list the amount of hours passed
-        since the beginning of the hour of the first time stamp.
+def set_up_time_steps(config):
+    """Create list from start, end date and resolution of the simulation period with all individual
+    time steps.
 
     Args:
-        time_stamps (list): Containing the time stamps as :obj: `datetime.datetime`.
+        config: (:obj: `config`): Configuration class containing the time/date parameters
 
     Returns:
-        list: Hours passed.
-    """
-    start = time_stamps[0]
-    hours_passed = []
-    for time_stamp in time_stamps:
-        delta = time_stamp - start
-        hour = delta.days * 24 + delta.seconds / 3600 + delta.microseconds / 3600 / 1000
-        # add offset
-        hour += start.minute / 60 + start.second / 3600 + start.microsecond / 3600 / 1000
-        hours_passed.append(hour)
-    return hours_passed
+        time_steps: (list): Contains time_steps in `datetime.datetime` format
 
-
-def allign_distribution(distr, first_time_stamp, last_time_stamp):
-    """Receives weekly distribution in hourly resolution besides a starting and an
-    ending time_stamp. Shifts and multiplies distribution to allign it to the
-     period of the time_stamps.
-
-    Args:
-        distribution (list): Containing hourly probabilities.
-        first_time_stamp (:obj: `datetime.datetime`): Start of period.
-        last_time_stamp (:obj: `datetime.datetime`): End of period.
-
-    Returns:
-        list: Containing the probabilities alligned to period.
-    """
-    starting_hour = first_time_stamp.weekday() * 24 + first_time_stamp.hour
-    period = last_time_stamp - first_time_stamp
-    # Upward estimate of the needed length of the distribution
-    total_hours = period.total_seconds() / 3600
-    total_weeks = math.ceil(total_hours / 168)
-
-    alligned_distribution = distr[starting_hour:] + distr * total_weeks
-    return alligned_distribution
-
-
-def create_vehicle_arrivals(config, time_steps):
-    """Creates vehicle arrival times.
-
-    Args:
-          time_steps (list): List containing all time steps as :obj: `datetime.datetime` object.
-
-    Returns:
-        list: Arrival times.
-
-    ToDo:
-        seeding seems to not work properly. With fixed seed small changes are still there,
-        changing the seed has a huge impact though.
-    """
-    arrival_distribution = config.arrival_distribution
-    num_charging_events = config.num_charging_events
-
-    # Rearrange arrival distribution so it starts with first hour of simulation time
-    arrival_distribution = allign_distribution(arrival_distribution, time_steps[0], time_steps[-1])
-
-    # Create distribution based on reordered arrival distribution
-    dist = distribution.EquallySpacedInterpolatedDistribution.linear(
-        list(zip(list(range(len(arrival_distribution))), arrival_distribution)), None)
-
-    # Calculate position of each time step at arrival distribution
-    corr_position = time_stamp_to_hours(time_steps)
-
-    # Get arrival probablity for each time step of the simulation
-    arrival_probability = []
-    for pos in corr_position:
-        arrival_probability.append(dist[pos])
-    # Normalize probability
-    cumsum = sum(arrival_probability)
-    arrival_probability = [x / cumsum for x in arrival_probability]
-
-    # Get on average num_charging_events arrivals per week
-    period = time_steps[-1] - time_steps[0]
-    num_weeks = period.total_seconds() / 7 / 24 / 3600
-    arrivals = []
-    corr_times = np.random.choice(corr_position, p=arrival_probability,
-                                  size=math.ceil(num_charging_events * num_weeks))
-
-    for corr_time in corr_times:
-        arrivals.append(time_steps[corr_position.index(corr_time)])
-
-    return sorted(arrivals)
-
-
-def simulate(config):
-    """Iterates over time and simulates the infrastructure.
-
-    Args:
-        config (:obj: `ElvisConfig`): User input.
     """
     start_date = config.start_date
     end_date = config.end_date
     resolution = config.resolution
 
-    # Create list containing all time steps as datetime object
+    # Create list containing all time steps as datetime.datetime object
     time_step = start_date
     time_steps = []
     while time_step <= end_date:
         time_steps.append(time_step)
         time_step += resolution
 
-    arrivals = create_vehicle_arrivals(config, time_steps)
-    print(arrivals)
+    return time_steps
+
+
+def handle_car_arrival(charging_points, event, waiting_queue):
+    """Connects car to charging point, to the queue or send it off.
+
+    Args:
+        charging_points: (list): Containing all charging points of the infrastructure as
+            :obj: `charging_point.ChargingPoint` instances.
+        event: (:obj: `charging_event.ChargingEvent`): Arriving charging event.
+        waiting_queue: (:obj: `queue.Queue`): Containing the waiting vehicles
+    """
+
+    # connect the arrival to an available connection point if possible
+    for charging_point in charging_points:
+        for connection_point in charging_point.connection_points:
+            connected_vehicle = connection_point.connected_vehicle
+            if connected_vehicle is None:
+                connection_point.connect_vehicle(event)
+                return waiting_queue
+
+    # if connection points unavailable put arrival to queue if not full and queue being considered
+    if not waiting_queue.full() and waiting_queue.maxsize > 0:
+        waiting_queue.put(event)
+    return waiting_queue
+
+
+def update_queue(waiting_queue, current_time_step, by_time):
+    """Removes cars that have spent their total parking time in the waiting queue and
+        therefore must leave.
+
+    Args:
+        waiting_queue: (:obj: `queue.Queue`): Charging events waiting for available
+        connection point.
+        current_time_step (:obj: `datetime.datetime`): current time step of the simulation.
+        by_time: (bool): True if cars shall be disconnected with respect to their parking time.
+        False if cars shall be disconnected due to their SOC target.
+
+    Returns:
+        updated_queue: (:obj: `queue.Queue`): Queue with removed cars that are overdue.
+    """
+
+    if by_time is False:
+        return waiting_queue
+
+    updated_queue = queue.Queue(waiting_queue.maxsize)
+    for i in range(waiting_queue.qsize()):
+        event = waiting_queue.get()
+        arrival_time = event.arrival_time
+        parking_time = datetime.timedelta(hours=event.parking_time)
+
+        # If waiting time is not longer than parking time remain in queue
+        if arrival_time + parking_time > current_time_step:
+            updated_queue.put(event)
+    return updated_queue
+
+
+def update_charging_points(charging_points, waiting_queue, current_time_step, by_time):
+    """Removes cars due to their parking time or their SOC limit and updates the connection points
+    respectively.
+
+    Args:
+        charging_points: (list): Contains :obj: `charging_point.ChargingPoints` instances.
+        waiting_queue: (:obj: `queue.Queue`): waiting vehicles/charging events.
+        current_time_step: (:obj: `datetime.datetime`): current time step.
+        by_time: (bool): Configuration class instance.
+    """
+    # if parking time is overdue: disconnect vehicle
+    # TODO: Too many nested blocks?
+    if by_time is True:
+        for charging_point in charging_points:
+            for connection_point in charging_point.connection_points:
+                connected_vehicle = connection_point.connected_vehicle
+                if connected_vehicle is not None:
+                    arrival_time = connected_vehicle['arrival_time']
+                    parking_time = datetime.timedelta(hours=connected_vehicle['parking_time'])
+
+                    if arrival_time + parking_time <= current_time_step:
+                        connection_point.disconnect_vehicle()
+                        # immediately connect next waiting car
+                        if waiting_queue.qsize() > 0:
+                            connection_point.connect_vehicle(waiting_queue.get())
+
+    # if SOC limit is reached: disconnect vehicle
+    if by_time is False:
+        for charging_point in charging_points:
+            for connection_point in charging_point.connection_points:
+                connected_vehicle = connection_point.connected_vehicle
+                if connected_vehicle is not None:
+                    soc = connected_vehicle.soc
+                    soc_target = connected_vehicle.soc_target
+
+                    if soc >= soc_target:
+                        connection_point.disconnect_vehicle()
+
+    return waiting_queue
+
+
+def simulate(config):
+    """Main simulation loop.
+    Iterates over simulation period and simulates the infrastructure.
+
+    Args:
+        config (:obj: `ElvisConfig`): User input.
+    """
+    # get list with all time_steps as datetime.datetime
+    time_steps = set_up_time_steps(config)
+    # create arrival times as sorted list of datetime.datetime
+    charging_events = create_charging_events(config, time_steps)
+    # Initialize waiting queue for cars at the infrastructure
+    waiting_queue = queue.Queue(maxsize=config.queue_length)
+    # set up charging points and their connection points
+    charging_points = set_up_charging_points(config.charging_points)
+
+    # loop over every time step
+    for time_step in time_steps:
+
+        # check if cars must be disconnected, if yes immediately connect car from queue if possible
+        waiting_queue = update_queue(waiting_queue, time_step, config.disconnect_by_time)
+
+        waiting_queue = update_charging_points(charging_points, waiting_queue,
+                                               time_step, config.disconnect_by_time)
+
+        # in case of multiple charging events in the same time step: handle one after the other
+        while len(charging_events) > 0 and time_step == charging_events[0].arrival_time:
+            waiting_queue = handle_car_arrival(charging_points, charging_events[0], waiting_queue)
+            # remove the arrival from the list
+            charging_events = charging_events[1:]
+
+    # TODO: assign power to every component
 
 
 class TestConfig:
     def __init__(self):
         self.start_date = datetime.datetime(2020, 1, 1)
-        self.end_date = datetime.datetime(2020, 12, 31, 23, 59)
-        self.resolution = datetime.timedelta(minutes=1)
+        self.end_date = datetime.datetime(2020, 1, 7, 23, 59)
+        self.resolution = datetime.timedelta(minutes=60)
 
-        self.num_charging_events = 1000
+        self.num_charging_events = 5
 
         self.arrival_distribution = [0 for x in range(168)] #[np.random.uniform(0, 1) for x in range(168)]
         self.arrival_distribution[8] = 1
+        self.arrival_distribution[10] = 1
         self.charging_events = 2
+        self.queue_length = 2
+        self.charging_points = [1, 1]
+        self.disconnect_by_time = True
 
 
 if __name__ == '__main__':
