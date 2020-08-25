41e1426fd762a6ff4657f2eb14eb8a23d4683612
19MM97 <moritz.markschlaeger@web.de>
2020-08-25 11:56:58 +0200

    Ádded some TODOs in module docstring

--- elvis/charging_point.py
+++ elvis/charging_point.py
@@ -4,15 +4,17 @@ A charging point is the point of coupling between connection point (connection t
 and the grid. In LV grids the charging point immediately connects to the grid in MV grids a
 transformer is needed in between.
 - The charging point may consist of multiple connection points.
-- It may be automated: After a car is charged it automatically connects itself to another car if
+TODO:
+    - It may be automated: After a car is charged it automatically connects itself to another car if
     available.
-- It may be inductive: No physical connection between car and connection point is needed:
+    - It may be inductive: No physical connection between car and connection point is needed:
     lower efficiency.
 - Or manual: User have to connect and disconnect their vehicle by hand: Longer phases of inactivity.
-- It can be operated with direct current or alternating current where different efficiencies
+TODO:
+    - It can be operated with direct current or alternating current where different efficiencies
     have to be considered.
-- Its power is the sum over the power of its connection points.
-- It is limited by its power limits where the lower limit has to be understood as at least the
+    - Its power is the sum over the power of its connection points.
+    - It is limited by its power limits where the lower limit has to be understood as at least the
     lower limit or zero.
 """
 
