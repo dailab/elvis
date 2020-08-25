59f6ea6ec3faafefc50483d215feb873d438b9d9
19MM97 <moritz.markschlaeger@web.de>
2020-08-25 11:41:45 +0200

    added new fields of ElvisConfig in ElvisConfigBuilder

--- elvis/config.py
+++ elvis/config.py
@@ -43,7 +43,6 @@ class ElvisConfig:
         self.queue_length = queue_length
         self.disconnect_by_time = disconnect_by_time
 
-
         # The cached results
         self._result = None
 
@@ -64,8 +63,10 @@ class ElvisConfig:
 
         pass
 
+
 class InvalidConfigException(Exception):
     pass
+
 
 class ElvisConfigBuilder:
     """Helper class to simplify the creation of ElvisConfig objects."""
@@ -92,6 +93,23 @@ class ElvisConfigBuilder:
         # The opening hours of the parking lot
         self.opening_hours = None
 
+        # Initial date the simulation starts off with (TODO: prob str to datetime.datetime conversion necessary)
+        self.start_date = None
+        # End of simulation period (TODO: prob str to datetime.datetime conversion necessary)
+        self.end_date = None
+        # Time in between two adjacent time steps (minutes, hours...). TODO: Define convention how to pass (hours, minutes, as datetime.timedelta object?)
+        self.resolution = None
+
+        # Number of charging events per week on average TODO: Is week still okay? or better day?
+        self.num_charging_events = None
+
+        # Maximum number of cars that can be simultaneously in the queue [0, infinity)
+        self.queue_length = None
+
+        # What is the condition to be met for disconnection at the connection point
+        # time or energy or?
+        self.disconnect_by_time = None
+
     def build(self):
         """Create the ElvisConfig with the passed parameters."""
 
@@ -111,6 +129,18 @@ class ElvisConfigBuilder:
             return "no scheduling policy specified"
         if self.opening_hours is None:
             return "no opening hours specified"
+        if self.start_date is None:
+            return 'no start date specified'
+        if self.end_date is None:
+            return 'no end date specified'
+        if self.resolution is None:
+            return 'no time resolution specified'
+        if self.num_charging_events is None:
+            return 'no number of charging events per week specified'
+        if self.queue_length is None:
+            return 'no queue length specified'
+        if self.disconnect_by_time is None:
+            return 'no disconnect by time specified'
 
         if not isinstance(self.scheduling_policy, sched.SchedulingPolicy):
             return "scheduling policy must be a subclass of elvis.sched.SchedulingPolicy"
@@ -161,3 +191,22 @@ class ElvisConfigBuilder:
         """Update the opening hours to use."""
 
         self.opening_hours = opening_hours
+
+    def with_time_params(self, time_params):
+        """Update the start date to use
+        TODO: Check if start date <= end date - resolution"""
+        self.start_date = time_params[0]
+        self.end_date = time_params[1]
+        self.resolution = time_params[2]
+
+    def with_num_charging_events(self, num_charging_events):
+        """Update the number of charging events to use."""
+        self.num_charging_events = num_charging_events
+
+    def with_queue_length(self, queue_length):
+        """Update maximal length of queue."""
+        self.queue_length = queue_length
+
+    def with_disconnect_by_time(self, disconnect_by_time):
+        """Update decision variable on how to disconnect cars."""
+        self.disconnect_by_time = disconnect_by_time
