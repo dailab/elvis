c586736ac7d19708977c81e1e95cb5f10fb3faa2
19MM97 <moritz.markschlaeger@web.de>
2020-08-25 18:58:21 +0200

    Added queue class as wrapper class for a list.

---
+++ elvis/queue.py
@@ -0,0 +1,50 @@
+"""Wrapper class to have a more simplistic object than queue.Queue.
+
+TODO: Question: Is setting self.next_leave to year 9999 okay? Does the job but looks weird.
+-> used to not have to check if len==0 or next_leave==None. see simulate.py, line: 89"""
+
+import datetime
+
+
+class Queue:
+    """Represents the waiting queue for vehicles."""
+    def __init__(self, maxsize=0, arrivals=None):
+
+        self.queue = []
+        self.next_leave = datetime.datetime(9999, 1, 1)
+        if arrivals is not None:
+            for arrival in arrivals:
+                self.queue.append(arrival)
+
+            self.determine_next_leaving_time()
+        self.maxsize = maxsize
+        # initialise with large enough date
+
+    def enqueue(self, item):
+        """Add item to queue if not already full."""
+        if self.size() < self.maxsize:
+            self.queue.append(item)
+
+    def dequeue(self):
+        """Get next item from queue if not empty."""
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+
+    def size(self):
+        """Get number of item in queue."""
+        return len(self.queue)
+
+    def determine_next_leaving_time(self):
+        """Determine the next departure time of all queued vehicles."""
+        if self.size() > 0:
+            leaving_times = []
+            for event in self.queue:
+                arrival_time = event.arrival_time
+                parking_time = datetime.timedelta(hours=event.parking_time)
+
+                leaving_times.append(arrival_time + parking_time)
+            self.next_leave = leaving_times.sort()[0]
+        else:
+            # if len = 0: Set next leave to be large enough date
+            self.next_leave = datetime.datetime(9999, 1, 1)
