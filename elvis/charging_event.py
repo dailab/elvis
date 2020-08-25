4ba7dad55b70cea6739a8325a65704d80a9563de
19MM97 <moritz.markschlaeger@web.de>
2020-08-25 11:52:00 +0200

    Change id from uuid to int with static counter field

--- elvis/charging_event.py
+++ elvis/charging_event.py
@@ -1,14 +1,16 @@
 
 from datetime import datetime
-from uuid import uuid4
 
 
 class ChargingEvent:
+    # static field incremented by 1 for every __init__
+    counter = 1
     """This class contains relevant parameters to describe a charging event.
 
     """
     def __init__(self, arrival_time):
-        self.id = datetime.now().strftime('%Y%m-%d%H-%M%S-') + str(uuid4())
+        self.id = str(ChargingEvent.counter)
+        ChargingEvent.counter += 1
         self.arrival_time = arrival_time
         self.parking_time = 5
         self.soc = 0.8
@@ -16,19 +18,17 @@ class ChargingEvent:
         self.vehicle_type = 'this should be the car'
 
     def __str__(self):
-        print_out = '----------' + '\n'
-        print_out += 'Charging_event: ' + self.id + '\n'
-        print_out += 'Arrival time: ' + str(self.arrival_time) + '\n'
-        print_out += 'Parking_time: ' + str(self.parking_time) + '\n'
-        print_out += 'SOC: ' + str(self.soc) + '\n'
-        print_out += 'SOC target: ' + str(self.soc_target) + '\n'
-        print_out += 'Connected car: ' + str(self.vehicle_type) + '\n'
-        print_out += '----------'
+        print_out = 'Charging_event: ' + self.id + ', '
+        print_out += 'Arrival time: ' + str(self.arrival_time) + ', '
+        print_out += 'Parking_time: ' + str(self.parking_time) + ', '
+        print_out += 'SOC: ' + str(self.soc) + ', '
+        print_out += 'SOC target: ' + str(self.soc_target) + ', '
+        print_out += 'Connected car: ' + str(self.vehicle_type)
 
         return print_out
 
     def to_dict(self):
-        """Make dict of the instance of :obj: charging_event.ChargingEvent
+        """Make dict of the instance of :obj: `charging_event.ChargingEvent`
             to speed up simulation.
 
         Returns:
