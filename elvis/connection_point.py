d4cafc24fb97fc031508e7f192f57bd812869b6f
19MM97 <moritz.markschlaeger@web.de>
2020-09-05 09:05:00 +0200

    Connection point is now subclass of InfrastructureNode. New class method has been added: charge_vehicle. Receives power and time resolution in order to perform the process of charging and change the vehicles SOC.

--- elvis/connection_point.py
+++ elvis/connection_point.py
@@ -1,35 +1,39 @@
 """Connection point representation.
 
-A connection point links the charging infrastructure to the charging vehicles.
+A connection point links the charging infrastructure to the vehicles to be charged.
 It is limited by its power limits where as the lower limit has to be understood as at least the
 lower limit or zero.
 
 """
 
+from elvis.infrastructure_node import InfrastructureNode
+from elvis.charging_point import ChargingPoint
 
-class ConnectionPoint:
+
+class ConnectionPoint(InfrastructureNode):
     """Represents a point of connection between :obj: `charging_point.ChargingPoint` and
     vehicles."""
 
     counter = 1
 
-    def __init__(self, limits, charging_point):
+    def __init__(self, min_power, max_power, parent):
         """Create a connection point given all parameters.
 
         Args:
-            limits: (tuple): (minimum power level as float, maximium power level as float)
-            charging_point: (:obj: `charging_point.ChargingPoint`): Charging point the connection
+            min_power: (float): Min charging power that has to be assigned if not 0.
+            max_power: (float): Max charging power that can be assigned.
+            parent: (:obj: `charging_point.ChargingPoint`): Charging point the connection
             point belongs to.
             """
-        self.id = 'Connection point: ' + str(ConnectionPoint.counter)
+        # A connection point must always be connected to a charging point
+        assert isinstance(parent, ChargingPoint)
+        identification = 'Connection_point_' + str(ConnectionPoint.counter)
         ConnectionPoint.counter += 1
 
-        self.max_power = limits[0]
-        self.min_power = limits[1]
+        # set min and max power
+        super().__init__(identification, min_power, max_power, parent=parent)
 
         self.connected_vehicle = None
-
-        self.charging_point = charging_point
 
     def __str__(self):
         printout = str(self.id) + ' ' + str(self.connected_vehicle)
@@ -46,3 +50,83 @@ class ConnectionPoint:
         """Set field connected_vehicle to None so connection point is available for
             vehicle connection."""
         self.connected_vehicle = None
+
+    def charge_vehicle(self, power, resolution):
+        """Charges the vehicle according to assigned power, capacity and efficiencies.
+        TODO: add efficiencies"""
+        vehicle_type = self.connected_vehicle['vehicle_type']
+        battery = vehicle_type.battery
+        hours = resolution.total_seconds()/3600
+        delta = power * hours / battery.capacity
+
+        self.connected_vehicle['soc'] += delta
+
+    def max_hardware_power(self):
+        """Calculate dependent on currently connected car the maximum power possible. Solely based
+            on the connection point and battery boundaries no charging point boundaries considered.
+            A further check whether this power exceeds the charging point boundaries has to be done
+            separately. At the same time it is not checked whether the soc_target is met."""
+
+        if self.connected_vehicle is not None:
+            vehicle = self.connected_vehicle
+            vehicle_type = vehicle['vehicle_type']
+            soc = vehicle['soc']
+            battery = vehicle_type.battery
+
+            max_power = min(self.max_power, battery.max_power_possible(soc))
+            return max_power
+        return None
+
+    def min_hardware_power(self):
+        """Calculate dependent on currently connected car the minimum power if not 0 needed
+            to start charging."""
+
+        if self.connected_vehicle is not None:
+            vehicle = self.connected_vehicle
+            vehicle_type = vehicle['vehicle_type']
+            soc = vehicle['soc']
+            battery = vehicle_type.battery
+
+            min_power = max(self.min_power, battery.min_power_possible(soc))
+            return min_power
+        return None
+
+    def power_to_charge_target(self, timedelta, soc_target):
+        """Calculate the average power needed in a time period to charge the battery of the
+            connected vehicle to a given soc target.
+
+        Args:
+            timedelta: (:obj: `datetime.timedelta`): Length of the timeperiod.
+            soc_target: (float): SOC target to be met.
+
+        Returns:
+            power_to_target: (float): Power needed to reach the given SOC target.
+            If soc_target > current_soc: return 0."""
+
+        if not self.check_soc(soc_target):
+            # TODO: Raise SOC out of boundaries error.
+            print('ERROR: SOC exceeded boundaries. SOC: ', soc_target)
+            pass
+
+        vehicle = self.connected_vehicle
+        current_soc = self.connected_vehicle['soc']
+
+        # if the battery is already charged further than the given SOC no power is needed
+        if current_soc > soc_target:
+            return 0
+
+        vehicle_type = vehicle['vehicle_type']
+        battery = vehicle_type.battery
+        battery_capacity = battery.capacity
+        timedelta_hours = timedelta.total_seconds() / 3600
+
+        power_to_target = (soc_target - current_soc) * battery_capacity / timedelta_hours
+
+        return power_to_target
+
+    @staticmethod
+    def check_soc(soc):
+        """Make sure soc is in between [0, 1]"""
+        if 0 <= soc <= 1.0:
+            return True
+        return False
