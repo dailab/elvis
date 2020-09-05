4b70fc1d3fc527dd19f04f6a81b81003a7f74635
19MM97 <moritz.markschlaeger@web.de>
2020-09-05 08:58:34 +0200

    Added a super() to __init__ method. ChargingPoint is not subclass of InfrstructureNode. Fixed values for min and max power have been changed and are no initialized by passing them to __init__.

--- elvis/charging_point.py
+++ elvis/charging_point.py
@@ -18,10 +18,11 @@ TODO:
     lower limit or zero.
 """
 
-import connection_point
+#from elvis.connection_point import ConnectionPoint
+from elvis.infrastructure_node import InfrastructureNode
 
 
-class ChargingPoint:
+class ChargingPoint(InfrastructureNode):
     """Models a charging point of the charging ingrastructure.
 
     TODO:
@@ -30,18 +31,15 @@ class ChargingPoint:
 
     counter = 1
 
-    def __init__(self, num_connection_points):
+    def __init__(self, min_power, max_power, parent):
         # id
-        self.id = 'Charging point: ' + str(ChargingPoint.counter)
+        identification = 'Charging point_' + str(ChargingPoint.counter)
         ChargingPoint.counter += 1
 
         # power limits
-        self.min_charging_power = 10
-        self.max_charging_power = 20
+        super().__init__(identification, min_power, max_power, parent=parent)
 
-        self.connection_points = self.set_connection_points_static(num_connection_points)
-
-        # working princibple
+        # working principle
         self.manual = True
         self.automated = False
         self.inductive = False
@@ -51,96 +49,23 @@ class ChargingPoint:
 
         self.efficiency = 1
 
-    def set_connection_points_static(self, n):
-        """Create n connection points.
-        With static values for power limits
+    def __str__(self):
+        return str(self.id)
 
-        Args:
-            n: (int): Number of connection points to be created.
-
-        Returns:
-            connection_points: (list): Containing the connection points as instances from
-            :obj: `ConnectionPoint`.
-        """
-        connection_points = []
-
-        while n > 0:
-            n -= 1
-            connection_points.append(connection_point.ConnectionPoint((2, 5), self))
-        return connection_points
-
-
-# def defineChargingPower(cp, powerAvailable):
-#     ev = cp.connectedEv
-#     min = max(ev.powerMin, cp.powerMin)
-#     max = min(ev.powerMax, cp.powerMax)
-#     # The power must not exceed any limits
-#     if powerAvailable > max:
-#         power = max
-#     elif powerAvailable < min:
-#         power = 0
-#     else:
-#         power = powerAvailable
-#
-#     # The power can't overload the EVBattery
-#     # Integral of power over time = Energy
-#     if convertPowerToEnergy(power, timeStepOfSimulation) + ev.currentEnergy > ev.capacity:
-#         power = convertEnergyToPower(EnergyNeededInEV, timeStep)
-#
-#     return power
-#
-#
-# # Give maximum or maximum possible with what is left in battery but not less than minimum.
-# def getPowerFromStationaryBattery():
-#     power = min(battery.maxPower, convertEnergyToPower(battery.currentEnergy, timeStep)
-#     if power < battery.minPower:
-#         power = 0
-#     return power
-#
-#
-# # This is the same procedure for every charging process (stationary or mobile battery) we could use a charging function
-# # working for both
-# def chargeStationaryBattery(power):
-#     if power < battery.minPower:
-#     # do not charge
-#     elif power > battery.maxPower:
-#     # charge with max power
-#     else:
-#
-#
-# # charge with available power
-#
-#
-# arrivals = arrivalsDistribution(amountOfArrivals)
-#
-# for t in simulation time:
-#     if t in arrivals:
-#         ev = create_ev(evDistribution)
-#         for cp in chargingPoints:
-#             if cp.connectedEV is not None:
-#                 connect
-#                 ev
-#         else ev to queue if queue has empty spot
-#     # Usually no stationary battery with UC control strategy, but would be easily possible
-#     powerAvailable = transformerLimit[t] - preload[t] + getPowerFromStationaryBattery()
-#
-#     # Loop over chargingpoints
-#     for cp in chargingPoints:
-#         if cp.connectedEV == None
-#             ev = cp.connectedEV
-#         if ev.parkingTime < t - ev.arrivalTime:
-#             ev.disconnect()
-#         else:  # (charge vehicle)
-#             power = defineChargingPower(cp, powerAvailable)
-#             powerAvailable -= power
-#             ev.battery.SOC += convertPowerToEnergy(power * ev.efficiency / ev.battery.capacity)
-#
-#             # Store Time Series Data:
-#             powerCp = power
-#             powerBattery = power * ev.efficiency
-#             SOCEV = ev.battery.SOC
-#
-#     # If not all power possible from transformer is needed the stationary battery if available can be charged
-#     If
-#     powerAvailable > 0:
-#     chargeStationaryBattery(powerAvailable)
+    # def set_connection_points_static(self, n):
+    #     """Create n connection points.
+    #     With static values for power limits
+    #
+    #     Args:
+    #         n: (int): Number of connection points to be created.
+    #
+    #     Returns:
+    #         connection_points: (list): Containing the connection points as instances from
+    #         :obj: `ConnectionPoint`.
+    #     """
+    #     connection_points = []
+    #
+    #     while n > 0:
+    #         n -= 1
+    #         connection_points.append(ConnectionPoint((2, 5), self))
+    #     return connection_points
