c0398ddfd3bdb73aebee0e9b13b69284b1e4e2be
19MM97 <moritz.markschlaeger@web.de>
2020-09-05 08:56:25 +0200

    Fixed an error that occured when initializing a battery and the unit types requested in the __init__ method could not be checked -> Import of elvis.units.py was missing

--- elvis/battery.py
+++ elvis/battery.py
@@ -1,8 +1,13 @@
+from elvis.units import Energy
+from elvis.units import Power
+
 
 class Battery:
     """Models a generic battery."""
 
-    def __init__(self, capacity: Energy, max_charge_power: Power, min_charge_power: Power, efficiency: float):
+    def __init__(self, capacity: Energy, max_charge_power: Power,
+                 min_charge_power: Power, efficiency: float):
+        """Create instance of Battery given all parameters."""
         # battery capacity (kWh)
         self.capacity = capacity
 
@@ -21,3 +26,32 @@ class EVBattery(Battery):
 
     def __init__(self, *args, **kwargs):
         super(EVBattery, self).__init__(*args, **kwargs)
+
+    def max_power_possible(self, current_soc):
+        """Return the max power that can be assigned to the battery in regard of the current
+            SOC.
+            As of implementation right now return power assignable to the car:
+                Max_power_at_battery_pins / conversion_efficiency.
+
+        Args:
+            current_soc: (float): Current state of charge: [0, 1]
+
+        Return:
+            max_power_possible: (float): Max assignable power.
+
+        TODO: Implement SOC dependence."""
+        return self.max_charge_power
+
+    def min_power_possible(self, current_soc):
+        """Return the min power that can be assigned to the battery in regard of the current
+        SOC.
+
+        Args:
+            current_soc: (float): Current state of charge: [0, 1]
+
+        Return:
+            max_power_possible: (float): Max assignable power.
+
+        TODO: Implement SOC dependence."""
+
+        return self.min_charge_power
