5e415327306ede51961df6dbea6380221eff0b98
19MM97 <moritz.markschlaeger@web.de>
2020-09-18 08:31:49 +0200

    Added a method to store the powers assigned to the vehicles to a dict in the result object. Should be used every time step in the simulation

--- elvis/result.py
+++ elvis/result.py
@@ -1,3 +1,6 @@
+import logging
+from elvis.connection_point import ConnectionPoint
+
 
 class ElvisResult:
     """Represents the result of an Elvis simulation. 
@@ -6,7 +9,26 @@ class ElvisResult:
     """
 
     def __init__(self):
-        pass
+        self.power_connection_points = {}
+
+    def store_power_connection_points(self, power_connection_points):
+        """Append the lists containing the powers assigned in every time step.
+            Called by simulate() each time_step.
+            Args:
+                - power_connection_points: (dict): Containing the connection points as keys as type
+                    :obj: `connection_point.ConnectionPoint`. Values are the powers to be assigned
+                    as float.
+        """
+        for connection_point in power_connection_points:
+            assert isinstance(connection_point, ConnectionPoint)
+            # append list containing the power of the connection points
+            try:
+                power = power_connection_points[connection_point]
+                self.power_connection_points[connection_point.id].append(power)
+            # if connection point not in dict add
+            except KeyError:
+                power = power_connection_points[connection_point]
+                self.power_connection_points[connection_point.id] = [power]
 
     def to_yaml(self):
         """Serialize this ElvisResult to a yaml string."""
