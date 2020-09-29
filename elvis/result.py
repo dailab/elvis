77faac09606a52ee4093186a591017b106e7bcc2
19MM97 <moritz.markschlaeger@web.de>
2020-09-29 21:23:13 +0200

    The dict storing the power assigned to the connection points is now not storing lists but dicts and values are only stored if they are not 0.

--- elvis/result.py
+++ elvis/result.py
@@ -11,24 +11,25 @@ class ElvisResult:
     def __init__(self):
         self.power_connection_points = {}
 
-    def store_power_connection_points(self, power_connection_points):
-        """Append the lists containing the powers assigned in every time step.
-            Called by simulate() each time_step.
+    def store_power_connection_points(self, power_connection_points, pos_current_time_stamp):
+        """Adds the key pos_current_time_stamp and the power assigned to the individual connection
+            point to self.power_connection_points if the assigned power is not 0.
+
             Args:
-                - power_connection_points: (dict): Containing the connection points as keys as type
+                power_connection_points: (dict): Containing the connection points as keys as type
                     :obj: `connection_point.ConnectionPoint`. Values are the powers to be assigned
                     as float.
+                pos_current_time_stamp: (int): Position of the current time stamp in the list
+                    containing all time stamps.
         """
         for connection_point in power_connection_points:
             assert isinstance(connection_point, ConnectionPoint)
-            # append list containing the power of the connection points
-            try:
-                power = power_connection_points[connection_point]
-                self.power_connection_points[connection_point.id].append(power)
-            # if connection point not in dict add
-            except KeyError:
-                power = power_connection_points[connection_point]
-                self.power_connection_points[connection_point.id] = [power]
+
+            power = power_connection_points[connection_point]
+            if power != 0:
+                if connection_point.id not in self.power_connection_points:
+                    self.power_connection_points[connection_point.id] = {}
+                self.power_connection_points[connection_point.id][pos_current_time_stamp] = power
 
     def to_yaml(self):
         """Serialize this ElvisResult to a yaml string."""
