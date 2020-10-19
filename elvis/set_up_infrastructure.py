d4136898acaf5cd112160c97eb890fc475087717
19MM97 <moritz.markschlaeger@web.de>
2020-10-19 17:15:15 +0200

    added method wallbox_infrastructure that generates an elvis conform infrastructure dict for charging points and connection points that are all equal. (Not used in simulation but can be used from FrontEnd). Should prob be moved to utility.

--- elvis/set_up_infrastructure.py
+++ elvis/set_up_infrastructure.py
@@ -52,3 +52,65 @@ def set_up_infrastructure(infrastructure):
     # transformer.draw_infrastructure()
 
     return connection_points
+
+
+def wallbox_infrastructure(num_stations, num_connections, power_connection, power_station,
+                           power_transformer, min_power_connection=0, min_power_station=0,
+                           min_power_transformer=0):
+    """Builds an Elvis conform infrastructure.
+
+    Args:
+        num_stations: (int): Number of charging stations.
+        num_connections: (int) Number of connection points per charging station.
+        power_connection: (int or float): Max power per connection point.
+        power_station: (int or float): Max power of the charging station.
+        power_transformer: (int or float): Max power of the transformer.
+        min_power_connection: (int or float): Minimum power (if not 0) for the connection point.
+        min_power_station: (int or float): Minimum power (if not 0) for the charging station.
+        min_power_transformer: (int or float) : Minimum power (if not 0) for the charging station.
+
+    Returns:
+        infrastructure: (dict)
+    """
+    # Validate Input
+    assert isinstance(num_stations, int), 'Only integers are allowed for the number of charging ' \
+                                          'stations.'
+    assert isinstance(num_connections, int), 'Only integers are allowed for the number of ' \
+                                             'connection points per charging station.'
+
+    msg_power_numeric = 'The power assigned to the infrastructure elements must be of type int or' \
+                        'float. The power must be bigger than 0.'
+    assert isinstance(power_connection, (int, float)), msg_power_numeric
+    assert isinstance(power_station, (int, float)), msg_power_numeric
+    assert isinstance(power_transformer, (int, float)), msg_power_numeric
+    assert isinstance(min_power_connection, (int, float)), msg_power_numeric
+    assert isinstance(min_power_transformer, (int, float)), msg_power_numeric
+    assert isinstance(min_power_station, (int, float)), msg_power_numeric
+
+    msg_min_max = 'The min power assigned to a infrastructure node must be lower than the ' \
+                  'max power.'
+    assert power_connection >= min_power_connection, msg_min_max
+    assert power_station >= min_power_station, msg_min_max
+    assert power_transformer >= min_power_transformer, msg_min_max
+
+    transformer = {'charging_points': [], 'id': 'transformer1',
+                   'min_power': min_power_transformer, 'max_power': power_transformer}
+
+    connection_point = {'min_power': min_power_connection, 'max_power': power_connection}
+
+    charging_station = {'min_power': min_power_station, 'max_power': power_station,
+                        'connection_points': []}
+
+    for i in range(num_connections):
+        connection_point_temp = connection_point
+        connection_point_temp['id'] = 'connection_point' + str(i + 1)
+        charging_station['connection_points'].append(connection_point_temp)
+
+    for i in range(num_stations):
+        charging_station_temp = charging_station
+        charging_station_temp['id'] = 'connection_point' + str(i + 1)
+        transformer['charging_points'].append(charging_station_temp)
+
+    infrastructure = {'transformers': [transformer]}
+
+    return infrastructure
