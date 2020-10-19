11354e4881b3f8a309ec6d35ce421c6bc850a2e6
19MM97 <moritz.markschlaeger@web.de>
2020-10-19 17:16:55 +0200

    Added to_dict method and probability field

--- elvis/vehicle.py
+++ elvis/vehicle.py
@@ -5,11 +5,20 @@ from elvis.battery import EVBattery
 class ElectricVehicle:
     """Models the charging behaviour of a specific EV model."""
 
-    def __init__(self, brand: str, model: str, battery: EVBattery):
+    def __init__(self, brand: str, model: str, battery: EVBattery, probability):
         self.brand = brand
         self.model = model
         self.battery = battery
+        self.probability = probability
 
     def __str__(self):
-        printout = 'Brand: ' + str(self.brand) + ', model: ' + str(self.model)
+        printout = self.brand + ', ' + self.model
         return printout
+
+    def to_dict(self):
+        dictionary = dict()
+        dictionary['brand'] = self.brand
+        dictionary['model'] = self.model
+        dictionary['battery'] = self.battery.to_dict()
+        dictionary['probability'] = self.probability
+        return dictionary
